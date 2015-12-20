0dec266d0aa4f5b14485c073d6c14e61206c180e
DD <dpy1123@126.com>
2015-12-20 18:24:40 +0800

    test

--- src/main/java/top/devgo/coupon/SMZDM.java
+++ src/main/java/top/devgo/coupon/SMZDM.java
@@ -1,28 +1,26 @@
 package top.devgo.coupon;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class SMZDM {
 	public static void main(String[] args) throws IOException {
 		CloseableHttpClient httpclient = HttpClients.createDefault();
-		URI uri = null;
-		try {
-			uri = new URIBuilder().setScheme("http")
-					.setHost("www.smzdm.com").setPath("/p1").build();
-		} catch (URISyntaxException e1) {
-			e1.printStackTrace();
-		}
-		HttpGet httpget = new HttpGet(uri);
+		
+		HttpGet httpget = new HttpGet("http://www.smzdm.com/p1");
+		httpget.setHeader("Host", "www.smzdm.com");
+		httpget.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36");
+		
 		CloseableHttpResponse response = null;
 		try {
 			response = httpclient.execute(httpget);
@@ -35,8 +33,47 @@ public class SMZDM {
 		try {
 			HttpEntity entity = response.getEntity();
 			if (entity != null) {
-				String html = EntityUtils.toString(entity, "UTF-8");
-				System.out.println(html);
+				String htmlStr = EntityUtils.toString(entity, "UTF-8");
+//				System.out.println(htmlStr);
+				
+				Document doc = Jsoup.parse(htmlStr);
+				Elements lists = doc.select("body > section > div.leftWrap > div.list:not(body > section > div.leftWrap > div.list.top.topSpace)");
+				for (Element element : lists) {
+					Element title = element.select("div.listTitle > h4 > a").first();
+					if(title != null){
+						String t = title.text();
+						String detailUrl = title.attr("href");
+						String p = null;
+						Element prise = title.child(0);
+						if(prise != null){
+							p = prise.text();
+							t = t.substring(0, t.lastIndexOf(p));
+						}
+						System.out.print(t+" || "+p+" || "+detailUrl);
+					}
+					
+					Element time = element.select("div.listRight > div.lrTop > span.lrTime").first();
+					if(time != null){
+						System.out.print(" || "+time.text());
+					}
+					
+					Element img = element.select("a > img").first();
+					if(img != null){
+						System.out.print(" || "+img.attr("src"));
+					}
+					
+					Element bref = element.select("div.listRight > div.lrInfo").first();
+					if(bref != null){
+						System.out.print(" || "+bref.text());
+					}
+					Element shoppingUrl = element.select("div.listRight > div.lrBot > div.botPart > div > a").first();
+					if(shoppingUrl != null){
+						System.out.print(" || "+shoppingUrl.attr("href"));
+					}
+					
+					System.out.print("\r");
+				}
+				
 			}	
 		} finally {
 			response.close();
